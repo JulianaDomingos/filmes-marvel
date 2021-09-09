80d7ee993d5ab422ef5b879cc7e5f6a2f07b5aa1
Juliana Domingos <julianadomingos17@gmail.com>
2021-09-09 20:20:28 -0300

    Adicionando e removendo itens dos meus favoritos

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
         FILMES
 
         <ul class="flex flex-wrap gap-4">
-          <li v-for="filme of listaDeFilmes" :key="filme.id">
+          <li v-for="(filme, index) of listaDeFilmes" :key="index">
             <img
               :src="'http://image.tmdb.org/t/p/w500/' + filme.poster_path"
               width="160px"
@@ -33,7 +33,10 @@
                 {{ filme.vote_average }}
               </div>
             </div>
-            <button class="bg-yellow-500 text-gray-100 w-40 text-sm p-1.5 mt-2">
+            <button
+              v-on:click="adicionarFavorito(index)"
+              class="bg-yellow-500 text-gray-100 w-40 text-sm p-1.5 mt-2"
+            >
               Adicionar aos favoritos
             </button>
           </li>
@@ -41,6 +44,40 @@
       </div>
       <div class="col-span-3 font-black text-gray-50 text-lg pt-5 ml-2 pl-2">
         MEUS FAVORITOS
+        <div>
+          <ul class="flex flex-wrap gap-4">
+            <li v-for="(filme, index) of listaDeFavoritos" :key="index">
+              <img
+                :src="'http://image.tmdb.org/t/p/w500/' + filme.poster_path"
+                width="160px"
+              />
+              <div
+                class="
+                  bg-gray-900
+                  text-gray-100
+                  w-40
+                  text-sm
+                  p-1.5
+                  break-words
+                  flex
+                  items-center
+                  h-20
+                "
+              >
+                <div class="w-40">{{ filme.title }}</div>
+                <div class="bg-gray-800 text-center p-1 w-9">
+                  {{ filme.vote_average }}
+                </div>
+              </div>
+              <button
+                v-on:click="removerFavorito(index)"
+                class="bg-yellow-500 text-gray-100 w-40 text-sm p-1.5 mt-2"
+              >
+                Remover dos favoritos
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -53,7 +90,17 @@ export default {
   data() {
     return {
       listaDeFilmes: [],
+      listaDeFavoritos: []
     };
+  },
+
+  methods: {
+    adicionarFavorito: function (indexFilme) {
+      this.listaDeFavoritos.push(this.listaDeFilmes[indexFilme])
+    },
+    removerFavorito: function(indexFilme) {
+      this.listaDeFavoritos.splice(indexFilme, 1)
+    }
   },
 
   mounted() {
